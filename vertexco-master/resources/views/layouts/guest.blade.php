08268dc737c12bab4174f0159bfed01e6123c00f
Mai Hoàng Đức <maihoangduc1103.ptts@gmail.com>
2025-04-02 23:26:36 +0700

    name web

--- vertexco-master/resources/views/layouts/guest.blade.php
+++ vertexco-master/resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Vertexco</title>
+    <title>Behind Office</title>
     <link href="{{ asset('resources/css/app.css') }}" rel="stylesheet">
     @yield('styles')
     <link rel="preconnect" href="https://fonts.googleapis.com">
